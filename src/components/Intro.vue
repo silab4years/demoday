2b191c36c887faef9941ce2da7c695d2bc3e0860
gnehs <jayuiop@gmail.com>
2021-11-14 14:26:33 +0800

    updated intro

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,10 +2,12 @@
   <section id="intro">
     <div class="intro-container">
       <div class="content">
-        <img src="../assets/intro/logo.png" />
+        <img class="subtitle" src="../assets/intro/subtitle.svg" />
+        <img class="main" src="../assets/intro/img_main_logo.svg" />
         <div class="links">
-          <a class="link-item" href="#">Gather 傳送門</a>
-          <a class="link-item" href="#">Gather 導覽說明</a>
+          <a class="link-item" href="https://gather.town/app/OpSG6nKGfIZ1MfCa/SDGS2021A" target="_blank">Gather 傳送門</a>
+          <a class="link-item" href="#guidebook">大會手冊</a>
+          <a class="link-item" href="https://www.accupass.com/event/2111010239578296478730" target="_blank">Accupass<br />搶先報名! </a>
         </div>
       </div>
     </div>
@@ -33,7 +35,7 @@ export default {}
 		@media (max-width: 1024px)
 			background-size: auto 40vh
 		@media (min-width: 1025px)
-			background-size: min(50vw, 700px) auto
+			background-size: max(40vw, 700px) auto
 			background-position: bottom right
 		.content
 			padding: 50px 0
@@ -41,28 +43,55 @@ export default {}
 			text-align: center
 			margin: 0 auto
 			@media (min-width: 1025px)
-				max-width: min(50vw, 500px)
+				width: 600px
+				max-width: 50vw
 				position: absolute
 				top: 50%
 				transform: translateY(-60%)
 				margin: initial
 			img
 				width: 100%
+				&.subtitle
+					width: 200px
+					margin-bottom: -20px
 			.links
+				display: grid
+				grid-template-columns: 180px 200px
+				grid-template-rows: 1fr 1fr
+				grid-template-areas: "gather accupass" "manual accupass"
+				grid-gap: 15px
+				margin-left: 50px
 				@media (max-width: 1024px)
+					margin-left: 0
 					display: flex
 					flex-direction: column
+					align-items: center
 				.link-item
 					background-color: #4ce7ff
-					border-radius: 100em
+					border-radius: 28px
 					color: #000
-					padding: 16px 32px
+					padding: 0 32px
 					text-decoration: none
 					display: inline-block
-					margin: 7px
+					min-height: 54px
 					font-weight: bold
+					display: inline-flex
+					align-items: center
+					justify-content: center
+					text-transform: uppercase
+					&:nth-child(1)
+						grid-area: gather
 					&:nth-child(2)
+						grid-area: manual
 						background: #FFD347
+					&:nth-child(3)
+						grid-area: 	accupass
+						background: #FF6900
+						color: #fff
+						line-height: 1.5
+
+					@media (max-width: 1024px)
+						width: 200px
 
 		.cover
 			img
